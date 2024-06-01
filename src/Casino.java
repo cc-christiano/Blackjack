36cb48c83e7afa08c16fb080c5dd75fb5f3987ef
cc-christiano <charlotte_christiano25@milton.edu>
2024-06-01 14:16:09 -0400

    I attempted to deal two cards to four players without re-dealing any cards.

--- src/Casino.java
+++ src/Casino.java
@@ -1,6 +1,8 @@
 public class Casino {
 
     public Card[] deck; // declare array of cards
+    public Player player1; //future me needs to make an array of Player called players
+    public Player[] players;
 
     public static void main(String[] args) {
         Casino myTable = new Casino();
@@ -24,35 +26,50 @@ public class Casino {
         }
 
         shuffle();
-        printDeck();
+//        printDeck();
+
+        //construct player1
+        player1 = new Player();
+        players = new Player[4];
+        //fill players array
+        for(int x = 0; x < 3; x++){
+            players[x] = new Player();
+        }
+
         deal();
     }
 
-        public void shuffle(){
-            // first, swap deck[0] and deck[1] cards
-            for(int m = 0; m < 52; m = m + 1) {
-                Card temp = deck[m];
-                int r = (int) (Math.random() * 52);
-                deck[m] = deck[r];
-                deck[r] = temp;
-            }
-            //eventually, loop through all deck
-            // swapping each card into a random index
+    public void shuffle(){
+        // first, swap deck[0] and deck[1] cards
+        for(int m = 0; m < 52; m = m + 1) {
+            Card temp = deck[m];
+            int r = (int) (Math.random() * 52);
+            deck[m] = deck[r];
+            deck[r] = temp;
+        }
+        //eventually, loop through all deck
+        // swapping each card into a random index
 
 
 
+    }
+
+    public void deal(){
+        for(int x = 0; x < 3; x++) {
+           for(int i = 0; i < 2; i++){
+               players[x].hand[i] = deck[(int)(Math.random()*52)];
+           }
         }
 
-        public void deal(){
-
+        for(int x = 0; x < 4; x++) {
+            players[x].printHand();
         }
 
-
-
+    }
 
 
     public void printDeck(){
-        for(int x = 0; x < 52; x = x + 1){
+        for(int x = 0; x < deck.length; x = x + 1){
             deck[x].printCard();
 
         }
--- src/Player.java
+++ src/Player.java
@@ -4,7 +4,16 @@ public class Player {
     public Card[] hand;
 
     public Player(){
+        hand = new Card[2];
+
 
     }
+
+    public void printHand(){
+        for(int x = 0; x < hand.length; x = x + 1){
+            hand[x].printCard();
+        }
+    }
+
 
 }
